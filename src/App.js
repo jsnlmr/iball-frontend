3c40b41344ab0dc34caa5f039c6ee8a78a2eb15a
Jason Campbell <jlcampbell86@gmail.com>
2019-01-10 10:47:13 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactMapBoxGL, { Layer, Feature, GeoJSONLayer } from 'react-mapbox-gl'
+import './App.css'
+
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      players: [],
+      courts: []
+    }
+  }
+
+
   render() {
+    const Map = ReactMapBoxGL({
+      accessToken: process.env.REACT_APP_MAPBOX_PUBLIC_ACCESS_TOKEN
+    })
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Map
+        style='mapbox://styles/mapbox/light-v9'
+        containerStyle={{
+          height: "100vh",
+          width: "100vw",
+        }}
+        center={[-77.032883, 38.898129]}
+      >
+        <Layer
+          type="circle"
+          id="courts"
+          layout={{ "icon-image": "marker-15" }}
+          paint={{'fill-color': 'red'}}>
+          <Feature coordinates={[-76.9954049, 38.8953954]}/>
+        </Layer>
+      </Map>
     );
   }
 }
